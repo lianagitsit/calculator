347451ebd41fb60f0dcb93e6694cdf0abfca4e84
Liana Mancini <lianamancini@Lianas-MBP.home>
2017-12-27 19:37:27 -0500

    subtraction, display sizing

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ $(document).ready(function(){
     var numStr = "";
     var targetValue;
     var addition = false;
+    var subtraction = false;
     var equal = false;
     var sum;
+    var difference;
     var pixel = 50;
 
     var display = document.getElementById("display");
@@ -18,24 +20,48 @@ $(document).ready(function(){
         display.textContent = numStr;
         console.log(numStr);
 
-        if (numStr.length > 10 && numStr.length < 13){
+        sizeDisplay();
+
+        /*if (numStr.length > 10 && numStr.length < 13){
             pixel -= 5;
         } else if (numStr.length > 12 && numStr.length < 15){ //>= 13 && numStr.length < 15){
             pixel -= 2.5;
-        } 
+        } */
 
         //display.style.fontSize = "40px";
-        display.style.fontSize = pixel + "px";
 
     }
 
     function addNumbers() {
         sum = a + b;
-        if (sum.toString().length > 14){
+        /*if (sum.toString().length > 12){
             display.style.fontSize = "20px";
-        }
+        }*/
         display.textContent = sum;
         console.log(a + " + " + b + " = " + sum);
+        sizeDisplay();
+    }
+
+    function subtractNumbers(){
+        difference = a - b;
+        /*if (difference.toString().length > 12){
+            display.style.fontSize = "20px";
+        }*/
+
+        display.textContent = difference;
+        sizeDisplay();
+    }
+
+    function sizeDisplay(){
+        if (display.textContent.toString().length > 10 && display.textContent.toString().length < 13) {
+            pixel -= 5;
+        } else if (display.textContent.toString().length > 12){ //&& display.textContent.toString().length < 15){
+            display.textContent = "ERR: OVERFLOW";
+            pixel = 35;
+        } 
+
+        display.style.fontSize = pixel + "px";
+
     }
 
     function allClear(){
@@ -76,10 +102,19 @@ $(document).ready(function(){
         } else if (targetValue === "="){
             equal = true;
             b = parseInt(numStr);
-            addNumbers();
+            if (addition === true){
+                addNumbers();
+            } else if (subtraction === true){
+                subtractNumbers();
+            }
             allClear();
         } else if (targetValue === "AC"){
             allClear();
+        } else if (targetValue === "-"){
+            subtraction = true;
+            a = parseInt(numStr);
+            numStr = "";
+            pixel = 50;
         }
     })
 })
