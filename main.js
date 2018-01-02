e5b2473f21a1b61ae61a971c4662ba0dedb9fda6
Liana Mancini <lianamancini@Lianas-MacBook-Pro.local>
2018-01-02 15:10:35 -0500

    chaining homogenous operations

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
     var sum;
     var difference;
     var pixel = 50;
+    var numbers = [];
 
     var display = document.getElementById("display");
 
@@ -18,7 +19,6 @@ $(document).ready(function(){
         //console.log("This element was clicked: " + targetValue);
         numStr += targetValue;
         display.textContent = numStr;
-        console.log(numStr);
 
         sizeDisplay();
 
@@ -33,23 +33,46 @@ $(document).ready(function(){
     }
 
     function addNumbers() {
-        sum = a + b;
+        console.log("Numbers to be added: " + numbers);
+        sum = numbers[0] + numbers[1];
+        //sum = a + b;
         /*if (sum.toString().length > 12){
             display.style.fontSize = "20px";
         }*/
         display.textContent = sum;
-        console.log(a + " + " + b + " = " + sum);
+        numbers[0] = sum;
+        numbers.pop();
+        console.log("Array after adding: " + numbers);
+        //console.log(a + " + " + b + " = " + sum);
+        //a = sum;
+        //addition = false;
         sizeDisplay();
     }
 
     function subtractNumbers(){
-        difference = a - b;
+        console.log("Numbers to be subtracted: " + numbers);
+        difference = numbers[0] - numbers[1];
         /*if (difference.toString().length > 12){
             display.style.fontSize = "20px";
         }*/
 
+        numbers[0] = difference;
+        numbers.pop();
+        console.log("Array after subtracting: " + numbers);
+
         display.textContent = difference;
         sizeDisplay();
+    }
+
+    function equals(){
+        equal = true;
+        numbers.push(parseInt(numStr));
+        //b = parseInt(numStr);
+        if (addition === true){
+            addNumbers();
+        } else if (subtraction === true){
+            subtractNumbers();
+        }
     }
 
     function sizeDisplay(){
@@ -77,6 +100,7 @@ $(document).ready(function(){
         equal = false;
         sum = 0;
         pixel = 50;
+        numbers = [];
 
         console.log("All cleared");
     }
@@ -95,24 +119,31 @@ $(document).ready(function(){
             }
         } else if (targetValue === "+"){
             addition = true;
-            a = parseInt(numStr);
-            console.log(a + " + ");
+            //if (numbers.length === 0){
+            numbers.push(parseInt(numStr));
+            //}
+            //console.log(numbers);
+
+            if (numbers.length === 2){
+                addNumbers();
+            }
+
+            //a = parseInt(numStr);
+            //console.log(a + " + ");
             numStr = "";
             pixel = 50;
         } else if (targetValue === "="){
-            equal = true;
-            b = parseInt(numStr);
-            if (addition === true){
-                addNumbers();
-            } else if (subtraction === true){
-                subtractNumbers();
-            }
-            allClear();
+            equals();
+            //allClear();
         } else if (targetValue === "AC"){
             allClear();
         } else if (targetValue === "-"){
             subtraction = true;
-            a = parseInt(numStr);
+            numbers.push(parseInt(numStr));
+            if (numbers.length === 2){
+                subtractNumbers();
+            }
+            //a = parseInt(numStr);
             numStr = "";
             pixel = 50;
         }
