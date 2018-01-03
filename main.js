ddbdd9bb71acf36cf8b6d40f97bc7ccfe9f90ca3
Liana Mancini <lianamancini@Lianas-MacBook-Pro.local>
2018-01-03 16:59:04 -0500

    chaining addition, subtraction, and equals

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
     console.log("ready");
 
-    var a, b;
-    var clicks = 0;
     var numStr = "";
     var targetValue;
     var addition = false;
@@ -17,45 +15,29 @@ $(document).ready(function(){
 
     function getNumber() {
         //console.log("This element was clicked: " + targetValue);
-        numStr += targetValue;
-        display.textContent = numStr;
-
-        sizeDisplay();
-
-        /*if (numStr.length > 10 && numStr.length < 13){
-            pixel -= 5;
-        } else if (numStr.length > 12 && numStr.length < 15){ //>= 13 && numStr.length < 15){
-            pixel -= 2.5;
-        } */
-
-        //display.style.fontSize = "40px";
-
+        equal = true;
+        if (!(targetValue >= 0 && targetValue <= 9)){
+            return;
+        } else {
+            numStr += targetValue;
+            display.textContent = numStr;
+            sizeDisplay();
+        }
     }
 
     function addNumbers() {
         console.log("Numbers to be added: " + numbers);
         sum = numbers[0] + numbers[1];
-        //sum = a + b;
-        /*if (sum.toString().length > 12){
-            display.style.fontSize = "20px";
-        }*/
         display.textContent = sum;
         numbers[0] = sum;
         numbers.pop();
         console.log("Array after adding: " + numbers);
-        //console.log(a + " + " + b + " = " + sum);
-        //a = sum;
-        //addition = false;
         sizeDisplay();
     }
 
     function subtractNumbers(){
         console.log("Numbers to be subtracted: " + numbers);
         difference = numbers[0] - numbers[1];
-        /*if (difference.toString().length > 12){
-            display.style.fontSize = "20px";
-        }*/
-
         numbers[0] = difference;
         numbers.pop();
         console.log("Array after subtracting: " + numbers);
@@ -65,26 +47,26 @@ $(document).ready(function(){
     }
 
     function equals(){
-        equal = true;
-        numbers.push(parseInt(numStr));
-        //b = parseInt(numStr);
+        //console.log("Numbers on equal: " + numbers)
+        if (numbers.length === 1){
+            numbers.push(parseInt(numStr));
+        }
         if (addition === true){
             addNumbers();
         } else if (subtraction === true){
             subtractNumbers();
         }
+        equal = false;
     }
 
     function sizeDisplay(){
         if (display.textContent.toString().length > 10 && display.textContent.toString().length < 13) {
             pixel -= 5;
-        } else if (display.textContent.toString().length > 12){ //&& display.textContent.toString().length < 15){
+        } else if (display.textContent.toString().length > 12){ 
             display.textContent = "ERR: OVERFLOW";
             pixel = 35;
         } 
-
         display.style.fontSize = pixel + "px";
-
     }
 
     function allClear(){
@@ -93,10 +75,9 @@ $(document).ready(function(){
             display.textContent = "0";
         }
 
-        a = 0;
-        b = 0;
         numStr = "";
         addition = false;
+        subtraction = false;
         equal = false;
         sum = 0;
         pixel = 50;
@@ -107,45 +88,72 @@ $(document).ready(function(){
 
     var numberPad = document.getElementById("number-pad");
 
+    var numberPadNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var numberPadOperands = ["+", "-", "x", "&#247;", "="];
+
     numberPad.addEventListener("click", function(e){
         targetValue = e.target.value;
-        // make a numbers array and use indexOf? an operators array?
+        // TODO: implement conditionals using indexOf in above arrays?
         if (targetValue >= 0 && targetValue <= 9){
             if (numStr.length >= 14){
                 console.log("pixels: " + pixel);
                 return;
             } else {
+                if (equal === false){
+                    numStr = "";
+                    numbers = [];
+                }
                 getNumber();
             }
         } else if (targetValue === "+"){
-            addition = true;
-            //if (numbers.length === 0){
-            numbers.push(parseInt(numStr));
-            //}
-            //console.log(numbers);
 
-            if (numbers.length === 2){
-                addNumbers();
+            // TODO: move this into its own block applied to all operands
+            if (numbers.length === 0 || (numbers.length === 1 && equal === true)){
+                numbers.push(parseInt(numStr));
+                numStr = "";
+                pixel = 50;    
+            } else if (numbers.length === 1 && equal === false){
+                numStr = "";
+                getNumber();
             }
 
-            //a = parseInt(numStr);
-            //console.log(a + " + ");
-            numStr = "";
-            pixel = 50;
+            if (numbers.length === 2){
+                if (subtraction === true){
+                    subtractNumbers();
+                } else {
+                    addNumbers();
+                }
+                numStr = "";
+                pixel = 50;    
+            }
+            subtraction = false;
+            addition = true;
         } else if (targetValue === "="){
+            equal = true;
             equals();
-            //allClear();
         } else if (targetValue === "AC"){
             allClear();
         } else if (targetValue === "-"){
-            subtraction = true;
-            numbers.push(parseInt(numStr));
-            if (numbers.length === 2){
-                subtractNumbers();
+            if (numbers.length === 0 || (numbers.length === 1 && equal === true)){
+                numbers.push(parseInt(numStr));
+                numStr = "";
+                pixel = 50;    
+            } else if (numbers.length === 1 && equal === false){
+                numStr = "";
+                getNumber();
             }
-            //a = parseInt(numStr);
-            numStr = "";
-            pixel = 50;
+
+            if (numbers.length === 2){
+                if (addition === true){
+                    addNumbers();
+                } else {
+                    subtractNumbers();
+                }
+                numStr = "";
+                pixel = 50;    
+            }
+            addition = false;
+            subtraction = true;
         }
     })
 })
